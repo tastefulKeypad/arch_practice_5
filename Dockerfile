d62c79e2fd7a1f9cfab2dc1536f681902c5db93c
tastefulKeypad <lpok@mail.ru>
2026-05-02 17:28:39 +0300

    Cleaned up main folder

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.12
-
-WORKDIR /app
-COPY . .
-RUN pip install -r requirements.txt
-
-EXPOSE 8000
--- securityConfig.py
+++
@@ -1,4 +0,0 @@
-# JWT token generation data
-SECRET_KEY = "a0c684ef7e4ba7ab2dfbf92591dc8bf8800891d9e04caeccc975b26c84fa587d"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
